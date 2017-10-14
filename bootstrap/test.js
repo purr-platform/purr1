791096a0ee97ca56d400481bb5096fbaf614d50a
Quildreen Motta <quildreen@gmail.com>
2017-10-14 11:32:31 -0300

    Remove old files

--- bootstrap/test.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const ohm = require('ohm-js');
-const contents = fs.readFileSync(path.join(__dirname, '../compiler/ast.purr'));
-const grammar = fs.readFileSync(path.join(__dirname, 'core-grammar.ohm'));
-
-const coreGrammar = ohm.grammar(grammar);
-console.log(Object.keys(coreGrammar.rules));
